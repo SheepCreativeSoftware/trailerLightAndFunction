340dbd5939262a296ee4f2273cf015cd9363139b
magraina <33145691+magraina@users.noreply.github.com>
2020-06-08 18:16:16 +0200

    v0.0.3 08.06.2020 - added directives for serial communication
    v0.0.3 08.06.2020
    - added directives for serial communication

--- trailerLightAndFunction.ino
+++ trailerLightAndFunction.ino
@@ -1,6 +1,6 @@
 /************************************ 
- * trailerLightAndFunction v0.0.2
- * Date: 09.05.2020 | 20-08
+ * trailerLightAndFunction v0.0.3
+ * Date: 08.06.2020 | 18:13
  * by M.Egner
  * <Trailer Module for Truck Light and function module>
  * Copyright (C) 2020 Marina Egner <info@sheepindustries.de>
@@ -21,20 +21,19 @@
  * Configuration Programm
  ************************************/
 #define ContryOption EU                   //Setup Region EU or US for Truck
-#define WireCom false                     //Activate Communication to other modules via I2C 
-#if (WireCom == true)
-#define TruckAdress 1                     //I2C Adress for Module: Truck
-#define BeaconAdress 2                    //I2C Adress for Module: Beacon Lights Extension
-#define TrailerAdress 3                   //I2C Adress for this Module: Trailer 
-#define ExtFunctionAdress 4               //I2C Adress for Module: Special function for example Servos Steering
+#define serialCom false                     //Activate Communication to other modules via I2C 
+#if (serialCom == true)
+	#define truckAdress 1                     	//Serial Adress for Module: Truck
+	#define beaconAdress 2                  	//Serial Adress for this Module: Beacon Lights Extension
+	#define trailerAdress 3                  	//Serial Adress for Module: Trailer 
+	#define extFunctionAdress 4               	//Serial Adress for Module: Special function for example Servos Steering
 #endif
-                        
+//Change this value for different debuging levels
+#define debugLevel 3						////1 = Status LED | >2 = Serial Monitor                        
 /************************************
  * Include Files
  ************************************/
-#if (WireCom == true)
-#include <Wire.h>                         //Include I2C Library
-#endif
+
 
 /************************************
  * Definition IO Pins
@@ -61,6 +60,34 @@
  * Definition and Initialisation 
  * Global Vars, Classes and Functions
  ************************************/
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#if (SERIAL_PORT_MONITOR != SERIAL_PORT_HARDWARE) 	//if serial ports are different then the arduino has more than one serial port
+	#ifndef SerialUSB								//if not allready defined
+		#define SerialUSB SERIAL_PORT_MONITOR		//then define monitor port
+	#endif
+#else
+	#if (debugLevel >1)								//if serial ports are the same debuging is not possible (for example on UNO)
+		#define debugLevel 1						//do not change!!!
+	#endif
+#endif
+#ifndef SerialHW									//if not allready defined
+	#define SerialHW SERIAL_PORT_HARDWARE			//then define hardware port
+#endif
+
 bool pulseStatus = false;
 unsigned long StatusPreviousMillis = 0;
 unsigned long blinkOnTime = 0;
@@ -72,8 +99,8 @@ int blink(unsigned int);
 
 void setup() {
 	// put your setup code here, to run once:
-	#if (WireCom == true)
-	Wire.begin(TrailerAdress);                 // join I2C bus (address optional for master)
+	#if (serialCom == true)
+	SerialHW.begin(115200);  // start Serial for Communication
 	#endif
 	// TODO: Setup IO pins
 }
@@ -84,11 +111,7 @@ void loop() {                             // put your main code here, to run rep
 
 	// Example For later Communication with other Module
 	// TODO: Setup Communication
-	#if (WireCom == true)
-	Wire.beginTransmission(8);             // transmit to device #8
-	Wire.write("Need to Setup COM");       // sends five bytes
-	Wire.endTransmission();                // stop transmitting
-	#endif
+	
 
 	controllerStatus(errorFlag);
 }
